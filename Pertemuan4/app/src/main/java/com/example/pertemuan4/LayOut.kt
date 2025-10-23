9eb1a06e92c9431bd5ed3cb8c7b2b205212ff065
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-10-23 16:47:32 +0700

    Add footer Box with copyright Text

--- Pertemuan4/app/src/main/java/com/example/pertemuan4/LayOut.kt
+++ Pertemuan4/app/src/main/java/com/example/pertemuan4/LayOut.kt
@@ -2,6 +2,7 @@ package com.example.pertemuan4
 
 import android.R.attr.text
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -66,6 +67,17 @@ fun ActivitasPertama(modifier: Modifier){
                             fontFamily = FontFamily.Cursive,
                             color = Color.White,
                             modifier = Modifier.padding(top = 10.dp)
+                        )
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.copy),
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                                .padding(bottom = 50.dp)
                         )
                     }
                 }
