ea98b7a34463725e476c51c610c470b7dc8a0841
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-10-23 15:45:49 +0700

    Membuat tata letak awal untuk layar utama

---
+++ Pertemuan4/app/src/main/java/com/example/pertemuan4/LayOut.kt
@@ -0,0 +1,25 @@
+package com.example.pertemuan4
+
+import android.R.attr.text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier
+        .padding(top = 100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+
+
+    }
+}
